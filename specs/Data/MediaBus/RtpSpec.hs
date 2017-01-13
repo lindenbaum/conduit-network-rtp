4b617b3ec9f0eea01300042af206872b790a7e43
Sven Heyll <sven.heyll@gmail.com>
2017-01-13 16:00:03 +0100

    WIP Add experimental Rtp code-thoughts

--- specs/Data/MediaBus/RtpSpec.hs
+++ specs/Data/MediaBus/RtpSpec.hs
@@ -2,6 +2,12 @@ module Data.MediaBus.RtpSpec ( spec ) where
 
 import           Test.Hspec
 import           Test.QuickCheck
+import           Data.Conduit
+import           Data.Word
+
+import Data.Default
+-- import Data.Conduit.Lift
+import Control.Monad.State.Strict
 
 spec :: Spec
 spec = do
@@ -27,10 +33,37 @@ spec = do
 -- Network Driven Rtp:
 -- per received network packet:
 --   Assign NTP timestamp
---   demultiplex sources based on SSRC
---   per SSRC:
---     create/use decoder for specific payload type
---     aggregate media data in ring
+--   stupid simple SSRC handling
+--      --> manage a list of MediaConduits one for each SSRC
+--      --> maybe enforce an upper limit on the number of concurrent SSRCs (maybe 2)
+--      --> ssrcDemux :: Monad m => (forall s. Default s => Ssrc -> Conduit MediaData (StateT s m) MediaData)
+--                               -> Conduit MediaData m MediaData
+newtype MediaStreamId = MkMediaStreamId { fromMediaStreamId :: Word32 }
+    deriving (Show)
+
+data MediaData = MkMediaData { mediaBuffer :: String }
+    deriving (Show)
+
+data MediaStream = MkMediaStream { mediaId      :: MediaStreamId
+                                 , streamPacket :: MediaData
+                                 }
+    deriving (Show)
+
+multiStream :: (Default s, Monad m)
+            => (MediaStreamId -> Conduit MediaData (StateT s m) MediaData)
+            -> Conduit MediaStream m MediaStream
+multiStream = undefined
+--      --> Then assume: type MediaFunction = forall m. Monad m => Conduit MediaData m MediaData
+--          and also a simple MediaMixer like:
+
+
+--
+--   Packet Loss Detection, Packet Ordering and Jitter
+--      -->
+--   create/use decoder for specific payload type
+--      --> (forall  s t. (Default s, Monad m) => (RtpPayloadType -> Conduit MediaData (StateT s m) MediaData) -> Conduit MediaData m MediaData)
+--     aggregate media data in a Set
+--        --> Sort by
 -- =================================
 
 -- =================================
