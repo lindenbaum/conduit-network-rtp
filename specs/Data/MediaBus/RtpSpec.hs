fe99b35f44fd61ce5cdfa25890040dd4470a1f77
Sven Heyll <sven.heyll@lindenbaum.eu>
2017-01-17 00:13:37 +0100

    WIP

--- specs/Data/MediaBus/RtpSpec.hs
+++ specs/Data/MediaBus/RtpSpec.hs
@@ -1,7 +1,8 @@
 module Data.MediaBus.RtpSpec ( spec ) where
 
--- -- import           Data.Conduit
+-- import           Data.Conduit
 import           Data.Word
+import           Data.Int
 import           Test.Hspec
 import           Test.QuickCheck
 import           Data.Function
@@ -12,9 +13,9 @@ import           Data.Time.Clock
 import           Data.Ord        ( Ordering, comparing )
 import qualified Data.Set        as Set
 
-
 spec :: Spec
 spec = do
+    sequentialSpec
     describe "incoming packet handling" $ do
         describe "packets with unknown SSRC" $ do
             it "just pushes packets to the end of the ring" $
@@ -32,6 +33,15 @@ spec = do
     describe "pulling packets" $ do
         it "returns all packets" $ property $ True
         it "fills the gaps with packet loss indications" $ property $ True
+
+sequentialSpec :: Spec
+sequentialSpec = do
+    describe "Monotonic instance" $ do
+        it "relativePosition ref . absolutePosition ref == id" $
+            property $
+                \(ref :: ReferencePosition (Monotonic Int8)) v ->
+                    relativePosition ref (absolutePosition ref v) `shouldBe`
+                        v
 
 -- =================================
 -- Network Driven Rtp:
@@ -53,13 +63,13 @@ type RtpMediaStreamEvent = MediaStreamEvent RtpSsrc RtpSequence RtpClock
 newtype RtpSsrc = MkRtpSsrc Word32
     deriving (Show, Eq)
 
-type RtpSequence = SequenceReference RtpSequenceNumber
+type RtpSequence = ReferencePosition RtpSequenceNumber
 
 newtype RtpSequenceNumber = MkRtpSequenceNumber (Monotonic Word16)
     deriving (Show, Bounded, Integral, Num, Enum, Real, Ord, Eq, IsSequential)
 
 mkRtpSequence :: RtpSequenceNumber -> RtpSequence
-mkRtpSequence = MkSequenceReference
+mkRtpSequence = MkReferencePosition
 
 type RtpClock = Clock RtpTimestamp
 
@@ -108,10 +118,10 @@ data RawRtpPacket = MkRawRtpPacket { rawRtpSequenceNumber :: RtpSequenceNumber
 --      activity detection, noise reduction, resampling, mix/combine with other
 --      stream media buffers
 --
-data StreamBufferQueue s p =
-      MkStreamBufferQueue { payloadQueue    :: Set.Set (Packet s p)
-                          , packetReference :: SequenceReference s
-                          , streamReference :: SequenceReference s
+data StreamBufferQueue tStream tPacket p =
+      MkStreamBufferQueue { payloadQueue    :: Set.Set (Packet (AbsolutePosition tPacket) p)
+                          , firstPacketTime :: ReferencePosition tPacket
+                          , streamReference :: ReferencePosition tStream
                           }
 
 newtype SessionId = MkSessionId (Word64, Word64)
@@ -124,8 +134,10 @@ newtype SessionSequenceNumber = MkSessionSequenceNumber Word64
     deriving (Show, Ord, Eq, Num, Integral, Enum, Real, Bounded)
 
 -- -----------------------------------------------------
+
+-- -----------------------------------------------------
 data MediaStreamEvent id s p =
-      Reconfigure id (SequenceReference s)
+      Reconfigure id (ReferencePosition s)
     | Process id (Packet s p)
     | Terminate id
     deriving (Show)
@@ -137,76 +149,60 @@ data Packet s p = MkPacket { sequenceNumber :: s
 
 -- -------------------------------------
 class IsSequential s where
-    getPosition :: SequenceReference s -> s -> s
-    comparePositions :: SequenceReference s -> s -> s -> Ordering
-    default comparePositions :: Ord s => SequenceReference s -> s -> s -> Ordering
-    comparePositions = comparing . getPosition
-    diffPositions :: SequenceReference s -> s -> s -> s
-    default diffPositions :: Num s => SequenceReference s -> s -> s -> s
-    diffPositions s = (-) `on` getPosition s
-    rebasePosition :: SequenceReference s -> s -> SequenceReference s -> s
-    default rebasePosition :: Integral s
-     => SequenceReference s -> s -> SequenceReference s -> s
-    rebasePosition yr y xr =
-        getPosition yr y + fromIntegral xr
+    absolutePosition :: ReferencePosition s -> s -> AbsolutePosition s
+    relativePosition :: ReferencePosition s -> AbsolutePosition s -> s
+    comparePositions :: ReferencePosition s -> s -> s -> Ordering
+    diffPositions :: ReferencePosition s -> s -> s -> AbsolutePosition s
 
 -- --------------------------------------
-
 instance (IsSequential s, IsSequential t) =>
          IsSequential (s, t) where
-    getPosition (MkSequenceReference (sr, tr)) (s, t) =
-        let sPos = getPosition (MkSequenceReference sr) s
-            tPos = getPosition (MkSequenceReference tr) t
+    absolutePosition (MkReferencePosition (sr, tr)) (s, t) =
+        let MkAbsolute sPos = absolutePosition (MkReferencePosition sr) s
+            MkAbsolute tPos = absolutePosition (MkReferencePosition tr) t
         in
-            (sPos, tPos)
-    comparePositions (MkSequenceReference (sr, tr)) (xs, xt) (ys, yt) =
-        let cmpS = comparePositions (MkSequenceReference sr) xs ys
-            cmpT = comparePositions (MkSequenceReference tr) xt yt
+            MkAbsolute (sPos, tPos)
+    comparePositions (MkReferencePosition (sr, tr)) (xs, xt) (ys, yt) =
+        let cmpS = comparePositions (MkReferencePosition sr) xs ys
+            cmpT = comparePositions (MkReferencePosition tr) xt yt
         in
             compare (cmpS, EQ) (EQ, cmpT)
-    diffPositions (MkSequenceReference (sr, tr)) (xs, xt) (ys, yt) =
-        ( diffPositions (MkSequenceReference sr) xs ys
-        , diffPositions (MkSequenceReference tr) xt yt
-        )
-    rebasePosition (MkSequenceReference (ysr, ytr)) (ys, yt) (MkSequenceReference (xsr, xtr)) =
-        ( rebasePosition (MkSequenceReference ysr) ys (MkSequenceReference xsr)
-        , rebasePosition (MkSequenceReference ytr) yt (MkSequenceReference xtr)
+    diffPositions (MkReferencePosition (sr, tr)) (xs, xt) (ys, yt) =
+        MkAbsolute ( fromAbsolutePosition $
+                       diffPositions (MkReferencePosition sr) xs ys
+                   , fromAbsolutePosition $
+                       diffPositions (MkReferencePosition tr) xt yt
+                   )
+    relativePosition (MkReferencePosition (xsr, xtr)) (MkAbsolute (ys, yt)) =
+        ( relativePosition (MkReferencePosition xsr) (MkAbsolute ys)
+        , relativePosition (MkReferencePosition xtr) (MkAbsolute yt)
         )
 
 -- --------------------------------------
+newtype ReferencePosition s = MkReferencePosition { referenceValue :: s }
+    deriving (Show, Bounded, Integral, Num, Enum, Real, Ord, Eq, Arbitrary)
 
-newtype SequenceReference s = MkSequenceReference { referenceValue :: s }
-    deriving (Show, Bounded, Integral, Num, Enum, Real, Ord, Eq)
-
-data AbsolutePosition s = MkAbsolute (SequenceReference s) s
-    deriving (Show)
-
-instance (Eq s, IsSequential s) =>
-         Eq (AbsolutePosition s) where
-    (MkAbsolute xr x) == (MkAbsolute yr y) =
-        x == rebasePosition yr y xr
-
-instance (Eq s, IsSequential s) =>
-         Ord (AbsolutePosition s) where
-    compare (MkAbsolute xr x) (MkAbsolute yr y) =
-        comparePositions xr x (rebasePosition yr y xr)
+newtype AbsolutePosition s = MkAbsolute { fromAbsolutePosition :: s }
+    deriving (Show, Bounded, Integral, Num, Enum, Real, Ord, Eq, Arbitrary)
 
 -- -------------------------------------
-
 newtype Monotonic i = MkMonotonic i
-    deriving (Eq, Show, Ord, Num, Real, Bounded, Enum, Integral)
+    deriving (Eq, Show, Ord, Num, Real, Bounded, Enum, Integral, Arbitrary)
 
 instance (Bounded i, Integral i) =>
          IsSequential (Monotonic i) where
-    getPosition (MkSequenceReference (MkMonotonic referenceValue)) (MkMonotonic x) =
+    absolutePosition (MkReferencePosition (MkMonotonic referenceValue)) (MkMonotonic x) =
         fromIntegral $
             if x < referenceValue
             then maxBound - (referenceValue - x)
             else x - referenceValue
+    comparePositions = comparing . absolutePosition
+    diffPositions s = (-) `on` absolutePosition s
+    relativePosition y xr = fromIntegral y + fromIntegral xr
 
 -- --------------------------------------
-data Clock t = MkClock { tickReference :: SequenceReference t
-                       , tickDuration  :: DiffTime
+data Clock t = MkClock { tickReference :: ReferencePosition t
+                       , tickDuration  :: NominalDiffTime
                        }
     deriving (Show)
 
@@ -215,11 +211,16 @@ newtype ClockRate = MkClockRate Word64
 
 fromRate :: ClockRate -> t -> Clock t
 fromRate ticksPerSecond ref =
-    MkClock (MkSequenceReference ref) (1 / fromIntegral ticksPerSecond)
+    MkClock (MkReferencePosition ref) (1 / fromIntegral ticksPerSecond)
 
-timeOf :: (Integral t, IsSequential t) => Clock t -> t -> DiffTime
-timeOf c t = fromIntegral (getPosition (tickReference c) t) * tickDuration c
+timeOf :: (Integral t, IsSequential t) => Clock t -> t -> NominalDiffTime
+timeOf c t = fromIntegral (absolutePosition (tickReference c) t) *
+    tickDuration c
 
-clockDiffTime :: (Integral t, IsSequential t) => Clock t -> t -> t -> DiffTime
+clockDiffTime :: (Integral t, IsSequential t)
+              => Clock t
+              -> t
+              -> t
+              -> NominalDiffTime
 clockDiffTime c t1 t0 = fromIntegral (diffPositions (tickReference c) t1 t0) *
     tickDuration c
