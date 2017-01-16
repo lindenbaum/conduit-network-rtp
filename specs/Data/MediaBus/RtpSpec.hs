030f144056a57b521454200ce783f89833f3b1a9
Sven Heyll <sven.heyll@gmail.com>
2017-01-16 21:32:20 +0100

    WIP

--- specs/Data/MediaBus/RtpSpec.hs
+++ specs/Data/MediaBus/RtpSpec.hs
@@ -1,15 +1,17 @@
 module Data.MediaBus.RtpSpec ( spec ) where
 
-import           Data.Conduit
+-- -- import           Data.Conduit
 import           Data.Word
 import           Test.Hspec
 import           Test.QuickCheck
 import           Data.Function
 import           Data.Time.Clock
-import           Data.Default
+-- -- import           Data.Default
 -- import Data.Conduit.Lift
-import           Control.Monad.State.Strict
-import           Data.Ord                   ( Ordering, comparing )
+-- -- import           Control.Monad.State.Strict
+import           Data.Ord        ( Ordering, comparing )
+import qualified Data.Set        as Set
+
 
 spec :: Spec
 spec = do
@@ -45,158 +47,179 @@ type RtpMediaStreamEvent = MediaStreamEvent RtpSsrc RtpSequence RtpClock
 
 -- mkRtpMediaStreamEvent :: RtpSequenceNumber -> RtpTimestamp -> RtpPayloadType -> RtpMediaStreamEvent
 -- mkRtpMediaStreamEvent = MkMediaStreamEvent
-
-type RtpMediaStream = MediaStream RtpSsrc RtpSequence RtpClock
-
-mkRtpMediaStream :: RtpSsrc -> RtpSequence -> RtpClock -> RtpMediaStream
-mkRtpMediaStream = MkMediaStream
-
+-- type RtpMediaEvent = MediaStream RtpSsrc RtpSequence RtpClock
+-- mkRtpMediaStream :: RtpSsrc -> RtpSequence -> RtpClock -> RtpMediaStream
+-- mkRtpMediaStream = MkMediaStream
 newtype RtpSsrc = MkRtpSsrc Word32
     deriving (Show, Eq)
 
-type RtpSequence = Sequence RtpSequenceNumber
+type RtpSequence = SequenceReference RtpSequenceNumber
 
-newtype RtpSequenceNumber = MkRtpSequenceNumber Word16
-    deriving (Show, Bounded, Integral, Num, Enum, Real, Ord, Eq)
+newtype RtpSequenceNumber = MkRtpSequenceNumber (Monotonic Word16)
+    deriving (Show, Bounded, Integral, Num, Enum, Real, Ord, Eq, IsSequential)
 
 mkRtpSequence :: RtpSequenceNumber -> RtpSequence
-mkRtpSequence = MkSequence
+mkRtpSequence = MkSequenceReference
 
 type RtpClock = Clock RtpTimestamp
 
-newtype RtpTimestamp = MkRtpTimestamp Word32
-    deriving (Show, Bounded, Integral, Num, Enum, Real, Ord, Eq)
+newtype RtpTimestamp = MkRtpTimestamp (Monotonic Word32)
+    deriving (Show, Bounded, Integral, Num, Enum, Real, Ord, Eq, IsSequential)
 
 mkRtpClock8kHz :: RtpTimestamp -> RtpClock
-mkRtpClock8kHz = mkClock 8000
+mkRtpClock8kHz = fromRate 8000
 
-
-data RtpPayloadType = G711U | G711A deriving (Show, Eq)
+data RtpPayloadType = G711U | G711A
+    deriving (Show, Eq)
 
 data RawRtpPacket = MkRawRtpPacket { rawRtpSequenceNumber :: RtpSequenceNumber
-                                   , rawRtpTimestamp :: RtpTimestamp
-                                   , rawRtpSsrc :: RtpSsrc
-                                   , rawRtpPayloadType :: RtpPayloadType
-                                   , rawRtpPayload :: String
-                                   } deriving Show
+                                   , rawRtpTimestamp      :: RtpTimestamp
+                                   , rawRtpSsrc           :: RtpSsrc
+                                   , rawRtpPayloadType    :: RtpPayloadType
+                                   , rawRtpPayload        :: String
+                                   }
+    deriving Show
 
--- rawRtpToMediaStream :: RawRtpPacket -> (RtpMediaStream, RtpMediaStreamEvent)
+-- rawRtpToEvent :: RawRtpPacket -> State RtpMediaStreamEvent
+-- -----------------------------------------------------
+--
+-- Order of RTP packets:
+-- Same SSRC: order by Rtp timestamp
+-- Different SSRC order by Ntp timestamp
+--
+-- stream buffer queue
+--   components:
+--    * the PacketReference
+--    * a bounded payload queue orderd by stream time stamp
+--    * the global start time stamp of the stream
+--
+--   operations:
+--    * gather media data packets
+--    * generate missing elements
+--
+-- media packet:
+--  components:
+--    * sequence number
+--    * timestamp
+--    * media data payload
+--
+--  operations:
+--    * filtering, transcoding, tone generation, dtmf/tone detection, voice
+--      activity detection, noise reduction, resampling, mix/combine with other
+--      stream media buffers
+--
+data StreamBufferQueue s p =
+      MkStreamBufferQueue { payloadQueue    :: Set.Set (Packet s p)
+                          , packetReference :: SequenceReference s
+                          , streamReference :: SequenceReference s
+                          }
+
+newtype SessionId = MkSessionId (Word64, Word64)
+    deriving (Show, Ord, Eq)
+
+newtype SessionTimestamp = MkSessionTimestamp Word64
+    deriving (Show, Ord, Eq, Num, Integral, Enum, Real, Bounded)
+
+newtype SessionSequenceNumber = MkSessionSequenceNumber Word64
+    deriving (Show, Ord, Eq, Num, Integral, Enum, Real, Bounded)
 
 -- -----------------------------------------------------
+data MediaStreamEvent id s p =
+      Reconfigure id (SequenceReference s)
+    | Process id (Packet s p)
+    | Terminate id
+    deriving (Show)
 
--- type MultiplexedMediaStreamEvent src seq clk =
--- -----------------------------------------------------
-data MediaStreamEvent sequence clock payload =
-      MkMediaStreamEvent { sequenceNumber :: SequenceElement sequence
-                         , timestamp      :: Timestamp clock
-                         , payload        :: payload
-                         }
-
-deriving instance
-         (Show (SequenceElement s), Show (SequenceElement c), Show p) =>
-         Show (MediaStreamEvent s c p)
-
-data MediaStream sourceId sequence clock =
-      MkMediaStream { sourceId :: sourceId
-                    , sequence :: sequence
-                    , clock    :: clock
-                    }
+data Packet s p = MkPacket { sequenceNumber :: s
+                           , payload        :: p
+                           }
     deriving (Show)
 
 -- -------------------------------------
-class IsSequence s where
-    type SequenceElement s
-    seqAdd :: Integral b => s -> b -> SequenceElement s -> SequenceElement s
-    seqPos :: Integral b => s -> SequenceElement s -> b
-
-seqCmp :: forall s.
-       IsSequence s
-       => s
-       -> SequenceElement s
-       -> SequenceElement s
-       -> Ordering
-seqCmp = comparing . (seqPos :: s -> SequenceElement s -> Integer)
-
-seqDiff :: (Integral b, IsSequence s)
-        => s
-        -> SequenceElement s
-        -> SequenceElement s
-        -> b
-seqDiff s = (-) `on` seqPos s
-
-newtype Sequence w = MkSequence w
-    deriving (Show)
-
-instance (Bounded w, Integral w) =>
-         IsSequence (Sequence w) where
-    type SequenceElement (Sequence w) = w
-    seqAdd _ x y = fromIntegral x + y
-    seqPos (MkSequence start) x =
-        fromIntegral (if x < start then maxBound - (start - x) else x - start)
+class IsSequential s where
+    getPosition :: SequenceReference s -> s -> s
+    comparePositions :: SequenceReference s -> s -> s -> Ordering
+    default comparePositions :: Ord s => SequenceReference s -> s -> s -> Ordering
+    comparePositions = comparing . getPosition
+    diffPositions :: SequenceReference s -> s -> s -> s
+    default diffPositions :: Num s => SequenceReference s -> s -> s -> s
+    diffPositions s = (-) `on` getPosition s
+    rebasePosition :: SequenceReference s -> s -> SequenceReference s -> s
+    default rebasePosition :: Integral s
+     => SequenceReference s -> s -> SequenceReference s -> s
+    rebasePosition yr y xr =
+        getPosition yr y + fromIntegral xr
 
 -- --------------------------------------
-class IsSequence c =>
-      IsClock c where
-    tickDuration :: c -> DiffTime
 
-type Timestamp c = SequenceElement c
+instance (IsSequential s, IsSequential t) =>
+         IsSequential (s, t) where
+    getPosition (MkSequenceReference (sr, tr)) (s, t) =
+        let sPos = getPosition (MkSequenceReference sr) s
+            tPos = getPosition (MkSequenceReference tr) t
+        in
+            (sPos, tPos)
+    comparePositions (MkSequenceReference (sr, tr)) (xs, xt) (ys, yt) =
+        let cmpS = comparePositions (MkSequenceReference sr) xs ys
+            cmpT = comparePositions (MkSequenceReference tr) xt yt
+        in
+            compare (cmpS, EQ) (EQ, cmpT)
+    diffPositions (MkSequenceReference (sr, tr)) (xs, xt) (ys, yt) =
+        ( diffPositions (MkSequenceReference sr) xs ys
+        , diffPositions (MkSequenceReference tr) xt yt
+        )
+    rebasePosition (MkSequenceReference (ysr, ytr)) (ys, yt) (MkSequenceReference (xsr, xtr)) =
+        ( rebasePosition (MkSequenceReference ysr) ys (MkSequenceReference xsr)
+        , rebasePosition (MkSequenceReference ytr) yt (MkSequenceReference xtr)
+        )
 
-timeOf :: IsClock c => c -> SequenceElement c -> DiffTime
-timeOf c t = fromInteger (seqPos c t) * tickDuration c
+-- --------------------------------------
 
-clockDiffTime :: IsClock c
-              => c
-              -> SequenceElement c
-              -> SequenceElement c
-              -> DiffTime
-clockDiffTime c t1 t0 = fromInteger (seqDiff c t1 t0) * tickDuration c
+newtype SequenceReference s = MkSequenceReference { referenceValue :: s }
+    deriving (Show, Bounded, Integral, Num, Enum, Real, Ord, Eq)
 
-data Clock t = MkClock { clockSequence     :: Sequence t
-                       , clockTickDuration :: DiffTime
+data AbsolutePosition s = MkAbsolute (SequenceReference s) s
+    deriving (Show)
+
+instance (Eq s, IsSequential s) =>
+         Eq (AbsolutePosition s) where
+    (MkAbsolute xr x) == (MkAbsolute yr y) =
+        x == rebasePosition yr y xr
+
+instance (Eq s, IsSequential s) =>
+         Ord (AbsolutePosition s) where
+    compare (MkAbsolute xr x) (MkAbsolute yr y) =
+        comparePositions xr x (rebasePosition yr y xr)
+
+-- -------------------------------------
+
+newtype Monotonic i = MkMonotonic i
+    deriving (Eq, Show, Ord, Num, Real, Bounded, Enum, Integral)
+
+instance (Bounded i, Integral i) =>
+         IsSequential (Monotonic i) where
+    getPosition (MkSequenceReference (MkMonotonic referenceValue)) (MkMonotonic x) =
+        fromIntegral $
+            if x < referenceValue
+            then maxBound - (referenceValue - x)
+            else x - referenceValue
+
+-- --------------------------------------
+data Clock t = MkClock { tickReference :: SequenceReference t
+                       , tickDuration  :: DiffTime
                        }
     deriving (Show)
 
-instance (Bounded t, Integral t) =>
-         IsSequence (Clock t) where
-    type SequenceElement (Clock t) = t
-    seqAdd = seqAdd . clockSequence
-    seqPos = seqPos . clockSequence
+newtype ClockRate = MkClockRate Word64
+    deriving (Show, Integral, Enum, Num, Real, Ord, Eq)
 
-instance (Bounded t, Integral t) =>
-         IsClock (Clock t) where
-    tickDuration = clockTickDuration
+fromRate :: ClockRate -> t -> Clock t
+fromRate ticksPerSecond ref =
+    MkClock (MkSequenceReference ref) (1 / fromIntegral ticksPerSecond)
 
-mkClock :: Integral a => a -> a -> Clock a
-mkClock ticksPerSecond startValue =
-    MkClock (MkSequence startValue) (1 / fromIntegral ticksPerSecond)-- ---------------------------------- WIP Below
-                                                                     -- -- | Stateful stream handling
-                                                                     -- multiStream :: (Default s, Monad m)
-                                                                     --             => (MediaStreamId -> Conduit MediaData (StateT s m) MediaData)
-                                                                     --             -> Conduit MediaStream m MediaStream
-                                                                     -- multiStream init = undefined
-                                                                     -- --      --> Then assume: type MediaFunction = forall m. Monad m => Conduit MediaData m MediaData
-                                                                     -- --          and also a simple MediaMixer like:
-                                                                     --
-                                                                     --   Packet Loss Detection, Packet Ordering and Jitter
-                                                                     --      -->
-                                                                     --   create/use decoder for specific payload type
-                                                                     --      --> (forall  s t. (Default s, Monad m) => (RtpPayloadType -> Conduit MediaData (StateT s m) MediaData) -> Conduit MediaData m MediaData)
-                                                                     --     aggregate media data in a Set
-                                                                     --        --> Sort by
-                                                                     -- =================================
-                                                                     -- =================================
-                                                                     -- Periodic Conduit over a Monadic action (the above for instance)
-                                                                     --   sleep for some time
-                                                                     --   initialize the start timestamp map
-                                                                     --   copy all elements from the ring into the aggregation buffer
-                                                                     --      if an element is "old" or missing
-                                                                     --           then remember the current start position in the gap list
-                                                                     --           else convert the start
-                                                                     -- ---------------------------------
-                                                                     -- =================================
-                                                                     -- Re-framer:
-                                                                     --  reframer :: Source m o -> (o -> m (Reframed o'))
-                                                                     -- A downstream event listener
-                                                                     --  encode
-                                                                     --  aggregate
-                                                                     --  frame/multiplex
+timeOf :: (Integral t, IsSequential t) => Clock t -> t -> DiffTime
+timeOf c t = fromIntegral (getPosition (tickReference c) t) * tickDuration c
+
+clockDiffTime :: (Integral t, IsSequential t) => Clock t -> t -> t -> DiffTime
+clockDiffTime c t1 t0 = fromIntegral (diffPositions (tickReference c) t1 t0) *
+    tickDuration c
